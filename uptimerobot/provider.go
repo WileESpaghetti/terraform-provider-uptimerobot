67b7e9c3048007d88a5410e3401190f62fff42c1
Lehman Black <WileESpaghetti@users.noreply.github.com>
2017-10-28 23:39:12 -0500

    Stub provider

---
+++ uptimerobot/provider.go
@@ -0,0 +1,9 @@
+package uptimerobot
+
+import (
+    "github.com/hashicorp/terraform/helper/schema"
+)
+
+func Provider() *schema.Provider {
+    return nil
+}
---
+++ uptimerobot/provider_test.go
@@ -0,0 +1,17 @@
+package uptimerobot
+
+import (
+	"testing"
+)
+
+func init() {
+}
+
+func TestProvider(t *testing.T) {
+}
+
+func TestProvider_impl(t *testing.T) {
+}
+
+func testAccPreCheck(t *testing.T) {
+}
